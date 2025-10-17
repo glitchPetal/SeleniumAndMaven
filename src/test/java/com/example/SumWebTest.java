3a29b36a26b69f3032a75df0bc197b0c3d84b194
Vidushi Singh <vidushisingh883@gmail.com>
2025-10-17 14:46:02 +0530

    third

--- src/test/java/com/example/SumWebTest.java
+++ src/test/java/com/example/SumWebTest.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,24 +29,31 @@ public class SumWebTest {
     }
 
     @Test
-    public void testSum() throws InterruptedException {
-        String url = "file:///C:/ProgramData/Jenkins/.jenkins/workspace/SeleniumWebSumTest/src/test/resources/sum.html";
+    public void testSum() throws InterruptedException, MalformedURLException {
+        // Dynamically get the file path to work anywhere
+        File file = new File("src/test/resources/sum.html");
+        String url = file.toURI().toURL().toString();
         driver.get(url);
 
+        // Wait for the page to load and the element to be present
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("num1")));
 
+        // Find elements
         WebElement num1 = driver.findElement(By.id("num1"));
         WebElement num2 = driver.findElement(By.id("num2"));
         WebElement calcBtn = driver.findElement(By.id("calcBtn"));
         WebElement result = driver.findElement(By.id("result"));
 
+        // Perform actions
         num1.sendKeys("10");
         num2.sendKeys("20");
         calcBtn.click();
 
-        Thread.sleep(1000); // wait for JS to update
+        // A short, explicit wait for the JavaScript to update the DOM
+        Thread.sleep(1000); 
 
+        // Get and verify the result
         String output = result.getText().trim();
         System.out.println("Output: " + output);
         assertEquals("Sum = 30", output);
@@ -52,7 +61,8 @@ public class SumWebTest {
 
     @After
     public void tearDown() {
-        if (driver != null) driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
-
